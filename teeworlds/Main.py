d8b751a9d165245aef61bd92da36493e6fb9d83b
plazmoid <kronos43@mail.ru>
2017-10-04 02:32:19 +0500

    Initial commit Physics, events, even 1 level

---
+++ teeworlds/Main.py
@@ -0,0 +1,96 @@
+import pygame
+import re
+from Entities import Player
+import Objects
+
+SCR_W_COEFF = 30
+SCR_H_COEFF = 16
+PF_SIZE = Objects.PLATFORM_SIZE
+SCR_SIZE = (PF_SIZE*SCR_W_COEFF, PF_SIZE*SCR_H_COEFF)
+
+class LevelBuilder:
+    
+    def __init__(self):
+        self.pattern = re.compile(r'-*?\d+?(?:\:-*?\d+\W|\W)')
+        self.blocks = {'#': Objects.Block,
+                       '!': Objects.JumperBlock}
+
+    def normalize(self, s_coord):
+        s_coord = int(s_coord)
+        if s_coord < 0:
+            s_coord += SCR_W_COEFF
+        return s_coord
+
+    def unpack(self, level):
+        self.result = {}
+        with open('lvl%d.txt' % level, 'r') as fi:
+            for row in fi:
+                self.result.clear()
+                for itm in self.pattern.findall(row):
+                    rawblock = itm[-1]
+                    itm = itm[:-1]
+                    if ':' in itm:
+                        pos = list(map(self.normalize, itm.split(':')))
+                        for i in range(pos[0], pos[1]+1):
+                            self.result[i] = rawblock
+                    else:
+                        self.result[self.normalize(itm)] = rawblock
+                yield self.result
+
+    def build(self, nlevel):
+        by = 0
+        for row in self.unpack(nlevel):
+            for bx, block in row.items():
+                bpoint = [bx*PF_SIZE, by*PF_SIZE]
+                self.blocks[block](bpoint)
+            by += 1
+
+
+level = LevelBuilder()
+level.build(1)
+hero = Player([100,200])
+mainloop = True
+
+window = pygame.display.set_mode(SCR_SIZE)
+screen = pygame.Surface(SCR_SIZE)
+    
+
+def eventsHandler():
+    global mainloop
+    e = pygame.event.poll()
+    if e.type == pygame.QUIT:
+        mainloop = False
+    elif e.type == pygame.KEYDOWN:
+        if e.key == pygame.K_LEFT:
+            hero.keydir[0] = -1
+        elif e.key == pygame.K_RIGHT:
+            hero.keydir[0] = 1
+        elif e.key == pygame.K_UP:
+            hero.keydir[1] = -1
+        elif e.key == pygame.K_ESCAPE:
+            mainloop = False
+        elif e.key == pygame.K_F1:
+            hero.rect.center = (100, 150)
+            hero.yvel = 0
+
+    elif e.type == pygame.KEYUP:
+        if e.key == pygame.K_LEFT:
+            hero.keydir[0] = 0
+        elif e.key == pygame.K_RIGHT:
+            hero.keydir[0] = 0
+        elif e.key == pygame.K_UP:
+            hero.keydir[1] = 0
+            
+    elif e.type == pygame.MOUSEBUTTONDOWN:
+        hero.moveAfterMouse(e.pos)
+    elif e.type == pygame.MOUSEMOTION:
+        hero.lookOnMouse(e.pos)
+
+while mainloop:
+    pygame.display.set_caption('dir: %s, xy: %s, vel: %s, %s' % (hero.dir, [hero.rect.x, hero.rect.y], [hero.xvel, round(hero.yvel, 2)], hero.onGround))
+    eventsHandler()
+    screen.fill(pygame.Color('white'))
+    Objects.ALL_OBJECTS.draw_n_update(screen)
+    window.blit(screen, (0,0))
+    pygame.display.flip()
+    pygame.time.wait(10)
